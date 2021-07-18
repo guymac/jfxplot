053bdbd2e551e35008fda6e4ccc35c5f0833d085
guymac <guymac@gmail.com>
2021-07-17 18:35:33 -0700

    set tooltips

--- src/main/java/guymac/PlotController.java
+++ src/main/java/guymac/PlotController.java
@@ -14,6 +14,7 @@ import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Tooltip;
 
 public class PlotController
 {
@@ -70,6 +71,7 @@ public class PlotController
         {
             var color = item.getYValue().doubleValue() >= 50 ? "blue" : "red";
             item.getNode().setStyle(String.format("-fx-background-color: %s", color));
+            Tooltip.install(item.getNode(), new Tooltip(item.getExtraValue().toString()));
         }
     }
 }
