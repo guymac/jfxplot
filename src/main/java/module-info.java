3e4c753b3dffac06f13c9f6b25433b821a73cdde
GuyMac <guymac@gmail.com>
2022-04-04 14:35:21 -0700

    added to build a runtime image

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Defines a module
+ */
+
+module guymac
+{
+    requires java.xml;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens guymac;
+}
