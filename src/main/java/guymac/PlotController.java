055e112e40e586b3a1d1cfa8bde452d6d67cbabb
GuyMac <guymac@gmail.com>
2022-09-07 16:16:29 -0700

    rm unused imports

--- src/main/java/guymac/PlotController.java
+++ src/main/java/guymac/PlotController.java
@@ -1,21 +1,8 @@
 package guymac;
 
-import java.io.IOException;
-import java.net.URL;
-
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Tooltip;
 
 public class PlotController
